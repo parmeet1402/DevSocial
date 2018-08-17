fe32f011ea3d7271c7e38c4fc129f77f859ba205
parmeet <parmeet.asi@gmail.com>
2018-08-17 17:28:32 +0530

    Added get and delete posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,17 +1,87 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+const Post = require("../models/Post"); //post nodel
+const validatePostInput = require("../../validation/post"); //post validation
 //set up a router
 const router = express.Router();
 
 // set the route
-
-// Route  --> GET api/posts/
+// Route  --> GET api/posts/test
 // Desc   --> Tests post route
 // Access --> Public
-router.get("/", (req, res) =>
+router.get("/test", (req, res) =>
   res.json({
     message: "posts works"
   })
+);
+
+// Route  --> GET api/posts/
+// Desc   --> get posts
+// Access --> Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err));
+});
+
+// Route  --> GET api/posts/:id
+// Desc   --> get one post
+// Access --> Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404));
+});
+
+// Route  --> POST api/posts/
+// Desc   --> create post
+// Access --> Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //check validation
+    if (!isValid) {
+      res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+// Route  --> DELETE api/posts/:id
+// Desc   --> delete a post
+// Access --> Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "user not authorized" });
+          }
+
+          //delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
 );
 
 // expose the router constant to server.js
