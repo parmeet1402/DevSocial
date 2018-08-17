7bb61568877ea7ab4d7be949bc8a1825d1e850f8
parmeet <parmeet.asi@gmail.com>
2018-08-17 18:08:59 +0530

    Added like and unlike posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,5 +84,66 @@ router.delete(
   }
 );
 
+// Route  --> POST api/posts/like/:id
+// Desc   --> like post
+// Access --> Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          }
+
+          // add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
+// Route  --> POST api/posts/unlike/:id
+// Desc   --> unlike post
+// Access --> Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            res.status(400).json({ notliked: "You have not liked this post" });
+          }
+
+          // remove user id to likes array
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          //save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
+
 // expose the router constant to server.js
 module.exports = router;
