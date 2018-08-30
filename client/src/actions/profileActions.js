8caec61aa107bcc34c4664ac73e83739489a50e3
parmeet <parmeet.asi@gmail.com>
2018-08-31 03:28:04 +0530

    added education and experience details in dashboard

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -85,6 +85,24 @@ export const deleteExperience = id => dispatch => {
     );
 };
 
+//delete education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //delete account and profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This cannot be undone!")) {
