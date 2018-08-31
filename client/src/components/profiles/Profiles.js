c4a2787fd7079319ddd7daf20d6e8cba3b4a2d05
parmeet <parmeet.asi@gmail.com>
2018-08-31 18:21:08 +0530

    added profileItems to profile component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,66 +2,56 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
-import {getProfiles} from '../../actions/profileActions';
-
+import { getProfiles } from "../../actions/profileActions";
+import ProfileItem from './ProfileItem';
 
 class Profiles extends Component {
-
-    componentDidMount(){
-        this.props.getProfiles();
-    }
-
-
-
-
-
-
-
-
-
-
-
+  componentDidMount() {
+    this.props.getProfiles();
+  }
 
   render() {
-      const {profiles, loading} = this.props.profile  ;
-      let profileItems;
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
 
-      if(profiles === null || loading){
-          profileItems = <Spinner />
-      }else{
-          if(profiles.length > 0){
-            <h1>PROFILES HERE</h1>
-            
-          }else{
-              profileItems = <h4>No Profiles found ..... </h4>
-          }
+    if (profiles === null || loading) {
+      profileItems = <Spinner />;
+    } else {
+      if (profiles.length > 0) { 
+        profileItems = profiles.map(profile => (
+            <ProfileItem key={profile._id} profile={profile} />
+        ));
+      } else {
+        profileItems = <h4>No Profiles found ..... </h4>;
       }
+    }
     return (
-            <div className="profiles">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h1 className="display-4 text-center">Developer Profiles</h1>
-                            <p className="text-center lead">
-                                Browse and connect with developers
-                            </p>
-                            {profileItems}
-                        </div>
-                    </div>
-                </div>
-
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="text-center lead">
+                Browse and connect with developers
+              </p>
+              {profileItems}
             </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 Profiles.propTypes = {
-    getProfiles: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-
 const mapStateToProps = state => ({
-    profile: state.profile
+  profile: state.profile
 });
-export default connect(mapStateToProps,{getProfiles})(Profiles);
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
