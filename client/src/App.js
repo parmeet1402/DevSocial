3edd0368bff494506f6b03d61cd1daf1d62d577f
parmeet <parmeet.asi@gmail.com>
2018-09-01 19:34:15 +0530

    added like and dislike feature for posts

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 import "./App.css";
 
 //check for token
@@ -89,6 +90,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
             </div>
             <Footer />
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import {ADD_POST} from '../..'
+import { ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST } from "../actions/types";
 
 const inititalState = {
   post: [],
@@ -8,6 +8,28 @@ const inititalState = {
 
 export default function(state = inititalState, action) {
   switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case DELETE_POST:
+      return{
+          ...state,
+          posts: state.posts.filter(post=> post._id !== action.payload)
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+
     default:
       return state;
   }
