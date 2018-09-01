e9756fb7a7940ebbcd6fb0aac2e906062b87bdc4
parmeet <parmeet.asi@gmail.com>
2018-09-01 16:53:55 +0530

    added add post action

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducers";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 /* const initialState = {};*/
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+import {ADD_POST} from '../..'
+
+const inititalState = {
+  post: [],
+  posts: [],
+  loading: false
+};
+
+export default function(state = inititalState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
