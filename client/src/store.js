66b6d9d97b576619c8c13e47b8d0a017a52fb673
parmeet <parmeet.asi@gmail.com>
2018-09-02 18:52:05 +0530

    ready for deploy

--- client/src/store.js
+++ client/src/store.js
@@ -18,11 +18,7 @@ const composeSetup =
 const store = createStore(
   rootReducer,
   initialState,
-
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeSetup(applyMiddleware(...middleware))
 );
 
 export default store;
