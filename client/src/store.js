54d7722df318963376ae3bba224992491a22692b
parmeet <parmeet.asi@gmail.com>
2018-09-02 18:12:49 +0530

    some tweaks

--- client/src/store.js
+++ client/src/store.js
@@ -7,6 +7,14 @@ const initialState = {};
 
 const middleware = [thunk];
 
+// Developer tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 const store = createStore(
   rootReducer,
   initialState,
